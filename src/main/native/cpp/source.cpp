8bc076b7668fbdc54aba8bf32a3a76da64d81192
jeffrey <jeffrey.fanjf@gmail.com>
2025-07-13 14:06:49 -0700

    test something else, bump version 2

--- src/main/native/cpp/source.cpp
+++ src/main/native/cpp/source.cpp
@@ -1,15 +1,3 @@
 #include "header.h"
 #include "driverheader.h"
 
-void func() {
-  std::cout << "Hello World from source.cpp\n";
-  c_doThing();
-}
-
-void Robot::RobotInit() {
-  std::cout << "Hello World from Robot::RobotInit\n";
-}
-
-void Robot::RobotPeriodic() {
-  std::cout << "Hello World from Robot::RobotPeriodic\n";
-}
